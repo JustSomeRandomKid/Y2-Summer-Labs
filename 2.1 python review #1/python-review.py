4225dec4f93fd95b4f92a8597c320e508ca5e5e8
UnknownDeveloperUnity <139048791+UnknownDeveloperUnity@users.noreply.github.com>
2024-07-14 23:43:46 +0300

    first lab work

---
+++ 2.1 python review #1/python-review.py
@@ -0,0 +1,49 @@
+import random as rnd
+
+temperatures = []
+
+for i in range(7):
+    temperatures.append(rnd.randint(26, 40))
+
+daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+goodDaysCount = 0
+numOfEvenDays = 0
+
+highestTemp = temperatures[0]
+highestTempDay = None
+
+lowestTemp = temperatures[0]
+lowestTempDay = None
+
+sumOfWeekTemp = 0
+
+aboveAvg = []
+
+for i in range(7):
+    sumOfWeekTemp += temperatures[i]
+
+    if temperatures[i] > highestTemp:
+        highestTemp = temperatures[i]
+        highestTempDay = daysOfTheWeek[i]
+
+    if temperatures[i] < lowestTemp:
+        lowestTemp = temperatures[i]
+        lowestTempDay = daysOfTheWeek[i]
+
+    if temperatures[i] % 2 == 0:
+        numOfEvenDays += 1
+
+    print(daysOfTheWeek[i], temperatures[i])
+
+avgWeekTemp = sumOfWeekTemp / 7
+
+for a in range(7):
+    if temperatures[a] > avgWeekTemp:
+        aboveAvg.append(daysOfTheWeek[a])
+
+print("\nShelly has", numOfEvenDays, "even days.")
+print("\nThe hottest temperature was", highestTemp, "on", highestTempDay)
+print("The lowest temperature was", lowestTemp, "on", lowestTempDay)
+print("\nThe average temprature was:",avgWeekTemp)
+print("The days with above average tempratures were:",aboveAvg)
