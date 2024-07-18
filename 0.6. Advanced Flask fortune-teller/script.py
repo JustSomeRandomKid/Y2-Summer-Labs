e9aae8863863da8a51de8579d7658e746ff0a3cd
UnknownDeveloperUnity <139048791+UnknownDeveloperUnity@users.noreply.github.com>
2024-07-18 18:54:14 +0300

    login_session lab work

--- 0.6. Advanced Flask fortune-teller/script.py
+++ 0.6. Advanced Flask fortune-teller/script.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template,request,redirect,url_for
+from flask import session as login_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY']="CS forever"
 
-@app.route('/home', methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    else:
+        login_session['userName'] = request.form['userName']
+        login_session['birth'] = request.form['birth']
+        return redirect(url_for('home'))
+
+@app.route('/home')
 def home():
     if request.method=='GET':
-        return render_template('home.html')
-    else:
-        birthMonth = request.form['textCont']
-        return redirect(url_for('fortune',birthM = birthMonth))
+        return render_template('home.html',username=login_session['userName'],birthMonth=login_session['birth'] )
     
-@app.route('/fortune/<birthM>')
-def fortune(birthM):
+@app.route('/fortune/<birth>')
+def fortune(birth):
     fortunes=['today will be the best day of your life!',
               'you will break a leg today ):',
               'someone will give you a very special gift soon',
@@ -24,10 +32,12 @@ def fortune(birthM):
               'you will have a great day today'
               ]
     finalFortune = fortunes[9]
-    if len(birthM) < 10:
-        finalFortune=fortunes[len(birthM)-1]
+    if len(birth) < 10:
+        finalFortune=fortunes[len(birth)-1]
 
-    return render_template("fortune.html",fortune = finalFortune)
+    login_session['fortune'] = finalFortune
+
+    return render_template("fortune.html",fortune = login_session['fortune'])
 
 
 if __name__ == '__main__':
